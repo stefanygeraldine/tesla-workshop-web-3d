4edbfe77026af40535b7f13864744e114b1ab371
stefany <stefany.carballo@n5now.com>
2021-08-09 22:05:45 -0300

    fixes colorPicker

--- src/components/ColorManager.js
+++ src/components/ColorManager.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as THREE from 'three'
 import state from '../state'
+import { SketchPicker, HuePicker, CirclePicker } from 'react-color';
 const styles ={
   contentColors: {
     position: 'absolute',
@@ -9,34 +10,44 @@ const styles ={
     top: 0,
     width: '100%',
     display: 'flex',
-    alignItems: 'end',
+    alignItems: 'center',
     justifyContent: 'center',
-  },
-  yellow:{
-    backgroundColor: 'yellow'
-  },
-  blue:{
-    backgroundColor: 'blue'
-  },
-  red:{
-    backgroundColor: 'red'
+    flexDirection:'column'
   }
 }
 
-const handledClick = e =>{
 
-  if ( !state.activeMesh ){
-    return
-  }
-
-  state.activeMesh.material.color = new THREE.Color(e.target.value)
-
-}
 
 const ColorManage = () => {
+  const [color, setColor] = useState('#000')
+
+  const handledClick = ({hex}) =>{
+
+    if ( !state.activeMesh ){
+      return
+    }
+
+    state.activeMesh.material.color = new THREE.Color(hex)
+    setColor(hex)
+
+  }
+
   return (
     <div style={styles.contentColors}>
-      <input type="color" onChange={handledClick}/>
+      <h1>Selecciona un Color</h1>
+      <div>
+        <HuePicker
+          color={ color }
+          onChangeComplete={ handledClick }
+        />
+      </div>
+      <div>
+        <CirclePicker
+          color={ color }
+          colors={['#000000', '#ecda1b', '#1b69ec','#db3e00','#909090', '#ffffff']}
+          onChangeComplete={ handledClick }
+        />
+      </div>
     </div>
   );
 };
